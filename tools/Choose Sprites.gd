608377862a2ddb0dc7c49b85ec1bc0653e3dab78
Alexg1247 <Alexg1247@users.github.noreply.com>
2023-04-14 19:08:11 -0600

    reverted the xml converter :3

--- tools/Choose Sprites.gd
+++ tools/Choose Sprites.gd
@@ -1,7 +1,8 @@
 extends Button
 
-@onready var file_dialog: FileDialog = $"../../file_dialog"
+@onready var file_dialog: FileDialog = $"../../FileDialog"
 
-func popup_shit() -> void:
-	file_dialog.size = Vector2(500, 500)
+func popup_shit():
 	file_dialog.popup_centered()
+	file_dialog.size = Vector2(500, 500)
+	file_dialog.position = Vector2(640 - 250, 360 - 250)
