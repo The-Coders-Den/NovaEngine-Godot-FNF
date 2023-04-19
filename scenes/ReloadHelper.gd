ae7267ccddf3cfcdafb15c08572753d545c9bfd7
swordcube <49110074+swordcube@users.noreply.github.com>
2023-04-19 00:31:54 -0500

    dying

--- scenes/ReloadHelper.gd
+++ scenes/ReloadHelper.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+# this is for making the mods menu work correctly
+# please do not change this
+
 func _ready():
 	await get_tree().create_timer(0.1).timeout
 	Global.switch_scene(Global.last_scene_path)
