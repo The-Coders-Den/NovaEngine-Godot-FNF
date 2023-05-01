1b2a6e07a1a0859f8e1c9b89ad1589615e612ee6
SrtHero278 <srtpro278@gmail.com>
2023-05-01 07:18:01 -0700

    whoop

--- scenes/gameplay/stages/mall.gd
+++ scenes/gameplay/stages/mall.gd
@@ -1,10 +1,6 @@
 extends Stage
 	
 @onready var da_boppers = [$"PB/0-33/UpperBop", $"PB/0-9/BottomBop", $"PB/1-0/Santa"]
-@onready var paraBG = $PB
-	
-func _process(delta):
-	pass
 	
 func on_beat_hit(beat:int):
 	for bopper in da_boppers:
