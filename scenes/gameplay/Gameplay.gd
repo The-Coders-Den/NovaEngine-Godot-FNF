eb02583bbe2381246152d79d38d3daa59783719f
swordcube <49110074+swordcube@users.noreply.github.com>
2023-05-06 19:22:12 -0500

    handle bpm changes in a less stupid way

--- scenes/gameplay/Gameplay.gd
+++ scenes/gameplay/Gameplay.gd
@@ -495,9 +495,6 @@ func section_hit(section:int):
 
 	if cam_switching:
 		update_camera(section)
-		
-	if SONG.sections[section].change_bpm:
-		Conductor.change_bpm(SONG.sections[section].bpm)
 
 	script_group.call_func("on_section_hit_post", [section])
 
